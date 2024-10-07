61d1075a5ef7c986c76599d479894ed049faad33
ciberSi4 <alex.baikonur@mail.ru>
2024-10-07 05:17:32 +0300

    Add files via upload

---
+++ module_6_3.py
@@ -0,0 +1,54 @@
+# Домашнее задание по теме "Множественное наследование"
+
+class Horse:
+
+    def __init__(self):
+        self.x_distance : int= 0
+        self.sound : str = 'Frrr'
+        super().__init__() # Отправка поиска по mro() в Eagle, что не нашло в Horse... В случае с sound -
+                           # принимает последнее значение, которое находит последним в Eagle
+
+    def run(self, dx):
+        self.x_distance += dx
+        return self.x_distance
+
+
+class Eagle:
+
+    def __init__(self):
+        self.y_distance : int = 0
+        self.sound : str = 'I train, eat, sleep, and repeat'
+
+    def fly(self, dy):
+        self.y_distance += dy
+        return self.y_distance
+
+
+class Pegasus(Horse, Eagle):
+    
+    def __init__(self):
+        super().__init__() # Отправка поиска по mro() в Horse
+        self.tuples: tuple = (self.x_distance, self.y_distance)
+
+    def move(self, dx, dy):
+        self.tuples = (super().run(dx), super().fly(dy)) # run находит по mro() в Horse, fly в Eagle
+        return self.tuples
+
+    def get_pos(self):
+        return self.tuples
+
+    def voice(self):
+        print(self.sound) # Первый sound находит в Horse, но в Horse отправка поиска в Eagle,
+                          # откуда и берётся выводимый sound
+
+
+
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()
